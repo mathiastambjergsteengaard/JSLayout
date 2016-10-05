c4fcd782993cce194d33515ce25821976e2f1065
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-10-05 15:17:44 +0200

    add to shopping cart

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( salme_37_5_road salme_37_5_feet )
+Rails.application.config.assets.precompile += Product.all.map(&:image_ref).compact
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
